6078464db5b0ac2bc7fbb25b9c520770b2bbf44c
VivekSharma811 <vs811861@gmail.com>
2021-08-24 18:21:07 +0530

    Create new Tour added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
 const express = require('express');
+const { dirname } = require('path');
+const { RSA_NO_PADDING } = require('constants');
 
 const app = express();
+
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from server', app: 'Natours'});
@@ -22,6 +26,32 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours
         }
     })
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+
+    const tour = tours.find();
+
+    res.status(200).json({
+        status: 'Success',
+        
+    })
+});
+
+app.post('/api/v1/tours', (req,res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
 });
 
 const port = 3000;
