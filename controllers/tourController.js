a64564e99bed8fe107d340befbab82301b3369ff
VivekSharma811 <vs811861@gmail.com>
2021-08-25 13:55:35 +0530

    Param Middleware added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 
+exports.checkId = (req, res, next, val) => {
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+};
+
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'Success',
@@ -17,13 +27,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
 
     const id = req.params.id * 1;
-    if(id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
     const tour = tours.find(el => el.id === id);
 
     res.status(200).json({
@@ -53,12 +56,6 @@ exports.createTour = (req,res) => {
 exports.updateTour = (req,res) => {
 
     const id = req.params.id * 1;
-    if(id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    };
 
     res.status(200).json({
         status: 'Success',
