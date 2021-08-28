ded796c2a5bd65cd0507b2c56f1e396513495bbf
VivekSharma811 <vs811861@gmail.com>
2021-08-28 15:04:04 +0530

    Mongoose added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({path: './config.env'});
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(() => console.log("Connected to DB"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
