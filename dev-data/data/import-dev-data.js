ee318295199de17bbc93a35f8d7a1d94e70c193d
VivekSharma811 <vs811861@gmail.com>
2021-08-29 12:21:19 +0530

    Script added

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true
+}).then(() => {
+console.log("Connected to DB")
+deleteData()
+});
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data loaded');
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data deleted');
+        importData()
+    } catch(err) {
+        console.log(err);
+    }
+}
