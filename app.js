ab16e7170ef55952491b9c1bd4fa7e3cef80c59f
VivekSharma811 <vs811861@gmail.com>
2021-09-08 13:58:11 +0530

    Global Error Added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const appError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -25,10 +28,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl}`
-    });
+    next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
+
+//Global Error Handling Middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
