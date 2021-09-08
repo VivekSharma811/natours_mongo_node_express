276c582a562d64550b9ae74513ee34cca785a336
VivekSharma811 <vs811861@gmail.com>
2021-09-08 18:34:20 +0530

    UncaughtException added

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log(err);
+    console.log('UncaughtException. Shutting Down....');
+    process.exit(1);
+});
+
 dotenv.config({path: './config.env'});
 const app = require('./app');
 
@@ -8,11 +14,17 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 
 mongoose.connect(DB, {
     useNewUrlParser: true
-}).then(() => {
-console.log("Connected to DB")
-});
+}).then(() => console.log("Connected to DB"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log('App running....');
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    console.log('UnhandledRejection. Shutting Down....');
+    server.close(() => {
+        process.exit(1);
+    });
 });
