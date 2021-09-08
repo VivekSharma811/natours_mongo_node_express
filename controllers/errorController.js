17bb4241d2bf79adef3d431c7649094eb2c64c40
VivekSharma811 <vs811861@gmail.com>
2021-09-08 17:47:53 +0530

    CastError handling done

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,48 @@
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
+const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
+        error: err,
+        message: err.message,
+        stack: err.stack
     });
+};
+
+const sendErrorProd = (err, res) => {
+    if(err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        console.error('Error: ', err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong'
+        });
+    }
+};
+
+module.exports = (err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+
+    if(process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if(process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+        if(err.name === 'CastError') {
+            error = handleCastErrorDB(error)
+        }
+        sendErrorProd(error, res);
+    }
+    
 };
