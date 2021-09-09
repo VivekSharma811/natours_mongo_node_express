672ab3bae132283af214fb730b1aa003ec62684b
VivekSharma811 <vs811861@gmail.com>
2021-09-09 18:01:04 +0530

    User roles and Authorization added

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
-        passwordChangedAt: req.body.passwordChangedAt
+        passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role
     });
 
     const token = signToken(newUser._id);
@@ -79,5 +80,16 @@ exports.protect = catchAsync(async (req, res, next) => {
         return next(new AppError('User recently changed password. Please login again', 401));
     }
 
+    req.user = user;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new AppError('You are not authorised to perform this action', 403));
+        }
+
+        next();
+    };
+};
