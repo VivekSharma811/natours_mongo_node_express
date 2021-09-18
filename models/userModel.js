2c67a869624622ef62297ba7559150647767e12d
VivekSharma811 <vs811861@gmail.com>
2021-09-18 12:33:18 +0530

    Delete user added

--- models/userModel.js
+++ models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -62,6 +67,11 @@ userSchema.pre('save', function(next) {
     if(!this.isModified('password') || this.isNew) return next();
 
     this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function(next) {
+    this.find({active: {$ne: false}});
     next();
 });
 
