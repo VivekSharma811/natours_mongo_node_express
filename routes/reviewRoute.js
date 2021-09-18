4154e706b30e31951bc9c6643ac43cb4ebc4448a
VivekSharma811 <vs811861@gmail.com>
2021-09-18 18:39:52 +0530

    Review by Tour added

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.route('/')
-    .get(authController.protect, reviewController.getAllReviews)
+    .get(reviewController.getAllReviews)
     .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
 module.exports = router;
